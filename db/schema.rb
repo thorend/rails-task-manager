3c72683326c253eaee3b2b6b874e9029001664b8
Thoren Depner <thorendepner@gmail.com>
2023-05-30 16:41:19 +0200

    challenge 2 completed partially, no refactoring, styling etc.

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_30_102120) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
